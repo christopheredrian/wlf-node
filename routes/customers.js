c2fd55c5e72b3ecd08d330c5c953cd0dced4ee9f
Christopher Espiritu <christopheredrian@gmail.com>
2017-05-08 06:16:45 +0800

    Bug Fix: list route

--- routes/customers.js
+++ routes/customers.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/list        ', function (req, res, next) {
+router.get('/list', function (req, res, next) {
     res.render('customers/index', {
         customers: [
             {
