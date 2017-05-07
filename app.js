0d478fa9de61fc692f4f2620c958a09428d82b81
Christopher Espiritu <christopheredrian@gmail.com>
2017-05-07 17:40:05 +0800

    Add: routes for registation

--- app.js
+++ app.js
@@ -8,6 +8,8 @@ var bodyParser = require('body-parser');
 var index = require('./routes/index');
 var users = require('./routes/users');
 var customers = require('./routes/customers');
+var registration = require('./routes/registration');
+
 
 var app = express();
 
@@ -27,6 +29,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', index);
 app.use('/users', users);
 app.use('/customers', customers);
+app.use('/registration', registration);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
