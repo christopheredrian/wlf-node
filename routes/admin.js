cf9edb9fae8fd13def4211215a5c8855d8a70ed2
Christopher Espiritu <christopheredrian@gmail.com>
2017-05-12 21:47:03 +0800

    Added dashboard functionality

--- routes/admin.js
+++ routes/admin.js
@@ -1,9 +1,32 @@
 var express = require('express');
+var registration = require('../models/registration');
+var customer = require('../models/customer');
+var provider = require('../models/providers');
+var invoice = require('../models/invoice');
 var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function (req, res, next) {
-    res.render('dashboard');
+    registration.getAll(function (err, results) {
+        var pendingRegistration = results.filter(function (obj) {
+            return obj.status === 'Pending';
+        });
+        customer.getAll(function (err, customerResults) {
+            provider.getAll(function (err, providerResults) {
+                invoice.getAll(function (err, invoiceResults) {
+                    var results = {
+                        pendingRegistrationCount: pendingRegistration.length,
+                        customerCount: customerResults.length,
+                        providerCount: providerResults.length,
+                        invoiceCount: invoiceResults.length
+                    };
+                    res.render('dashboard', results);
+                });
+            });
+        });
+
+    });
+
 });
 
 
