c5c07cd6e393d5ac63c3034a16705899eb9e223e
Christopher Espiritu <christopheredrian@gmail.com>
2017-05-12 23:03:32 +0800

    Add authentication and simple redirection condition

--- routes/login.js
+++ routes/login.js
@@ -1,4 +1,7 @@
 var express = require('express');
+var customer = require('../models/customer');
+var provider = require('../models/providers');
+
 var router = express.Router();
 
 router.get('/', function (req, res, next) {
@@ -11,12 +14,36 @@ router.post('/', function (req, res, next) {
     if (post.username === 'admin' && post.password === 'admin') {
         req.session.username = 1;
         res.redirect('/admin/registration/list');
-    } else if (false) {
-        // Use the registered database here, check type
     } else {
-        res.render('login', {
-            error: 'Invalid credentials!'
+        // Check on customers and providers
+        customer.usernameExists(post.username, function (err, customerExists) {
+            console.log('was here' + customerExists + ',' + post.username);
+            if (customerExists) {
+                console.log('a customer');
+                // send post request to php
+                // TODO
+                // redirect to php
+                res.send('Redirec to php')
+            } else {
+                // check if provider
+                provider.usernameExists(post.username, function (err, providerExists) {
+                    if (providerExists) {
+                        console.log('a provider');
+                        // send post request to Java
+                        // TODO
+                        // redirect to java
+                        res.send('Redirec to java')
+                    } else {
+                        console.log('invalid');
+                        res.render('login', {
+                            error: 'Invalid credentials!'
+                        });
+                    }
+                });
+
+            }
         });
+
     }
 });
 
